23c26d72bc5fae20e13a4c7e38926aa4b5480627
Siuari <siuaridamaceno@gmail.com>
2019-05-17 02:09:32 -0300

    inicado projeto usando api do github

---
+++ GitPages/wwwroot/js/Index.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+
+const pesq = document.getElementById("listar-btn");
+const pes = document.getElementById("pesquisa-text")
+const url = "https://api.github.com/search/repositories?q=user:Siuari"
+var tabela = document.getElementById("tabela");
+
+
+
+pesq.addEventListener('click', getRepos);
+
+function listarRepositorios(lista) {
+    for (let i = 0; i < lista.length; i++) {
+        var row = tabela.insertRow(1);
+        row.innerHTML = '<td>' + lista[i].name + '</td>' + '<td><a href="http://github.com/' + lista[i].full_name + '"/>'+lista[i].full_name+'</td>';
+    }
+}
+
+async function getRepos() {
+    const response = await fetch(url);
+    const result = await response.json();
+    listarRepositorios(result.items);
+    /*
+    $("#modal-mensagem").modal('show');
+    $("#mensagem").html(repos);
+    console.log(result);
+    console.log(result.items[0].name);
+    */
+}
--- GitPages/wwwroot/js/site.js
+++ GitPages/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+'use strict';
+const pesq = document.getElementById("pesquisa-btn");
+const pes = document.getElementById("pesquisa-text")
+
+pesq.addEventListener('click', function () {
+    $("#modal-mensagem").modal('show');
+})
+
